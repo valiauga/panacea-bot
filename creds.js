4a11ada47d60b730e7139227677d0c38b827a7e5
Lukas Valiauga <valiaugalukas@gmail.com>
2020-04-04 15:17:18 +0200

    Update creds.js

--- creds.js
+++ creds.js
@@ -1,6 +1,6 @@
 let creds = {
-    webhook: 'https://hooks.slack.com/services/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',
-    api: 'http://www.webcal.fi/cal.php?id=50&format=json&start_year=current_year&end_year=current_year&tz=UTC'
+    webhook: 'https://hooks.slack.com/services/xxxxxxxxxxxxxx', //Replace with your slack webhook!!!
+    api: 'http://www.webcal.fi/cal.php?id=50&format=json&start_year=current_year&end_year=current_year&tz=UTC' //Calendar call, its a big one (Don't use more than once a day!!!!
 }
 
 module.exports.endPoint = creds;
